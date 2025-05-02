737d9491c557ee8e3771c1960189a62fee8a7e9f
Dmitry Ovsyanko <do_@pochta.ru>
2025-05-02 22:08:31 +0300

    cluster aware index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const process     = require ('process')
+const cluster     = require ('node:cluster')
+const process     = require ('node:process')
 const conf        = require ('./lib/Conf')
 const logger      = require ('./lib/Logger.js') (conf)
 const Application = require ('./lib/Application.js')
@@ -20,12 +21,53 @@ function exit () {
 
 }
 
-for (const signal of ['SIGTERM', 'SIGINT', 'SIGBREAK']) process.on (signal, exit)
+async function startWorkers () {
 
-async function main () {
+    cluster.on ('message', (w, o) => {if ('message' in o && 'level' in o) logger.log (o)})
+
+    let N = require ('node:os').availableParallelism (); if (N > 1) N --
+
+    return new Promise (ok => {
+
+        let online = 0, onOnline = () => {
+
+            if ((++ online < N)) return
+
+            cluster.off ('online', onOnline)
+
+            ok ()
+
+        }
+
+        cluster.on ('online', onOnline)
+
+        for (let i = 0; i < N; i ++) cluster.fork ()
+
+    })
+
+}
+
+async function main () {    
+    
     app = new Application (conf, logger)
-    await app.do ('init')
-    app.start ()
+
+    if (cluster.isPrimary) {
+
+        for (const signal of ['SIGTERM', 'SIGINT', 'SIGBREAK']) process.on (signal, exit)
+
+        if (!conf.noCluster) await startWorkers ()
+
+        await app.do ('init')
+
+        app.start ()
+    
+    } 
+    else {
+
+        process.on ('message', request => app.process (request))
+    
+    }
+    
 }
 
 main ().then (_=>_ , _ => console.log (_))
